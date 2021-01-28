448f91f13199cd7aac633261c9d648349bcee00d
bbish <brenana88@gmail.com>
2021-01-28 16:26:02 -0500

    "list of tags show on all-hashtag-template"

--- src/main/java/com/survivingcodingbootcamp/blog/Populator.java
+++ src/main/java/com/survivingcodingbootcamp/blog/Populator.java
@@ -27,6 +27,13 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Hashtag hashtag1 = new Hashtag("#OOP");
+        hashtagStorage.save(hashtag1);
+        Hashtag hashtag2 = new Hashtag("#Testtest");
+        hashtagStorage.save(hashtag2);
+        Hashtag hashtag3 = new Hashtag("#TDD");
+        hashtagStorage.save(hashtag3);
+
         Topic topic1 = new Topic("Learning TDD");
         topicStorage.save(topic1);
 
@@ -35,21 +42,21 @@ public class Populator implements CommandLineRunner {
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.");
+                "est laborum.", hashtag3);
         postStorage.save(post1);
         Post post2 = new Post("Test the Fear Away", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.");
+                "est laborum.", hashtag2);
         postStorage.save(post2);
         Post post3 = new Post("Unit Tests and You", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
-                "est laborum.");
+                "est laborum.", hashtag2, hashtag3);
         postStorage.save(post3);
         Topic topic2 = new Topic("Battling Imposter Syndrome");
         topicStorage.save(topic2);
@@ -58,10 +65,7 @@ public class Populator implements CommandLineRunner {
         Topic topic4 = new Topic("Object Oriented Programming and You");
         topicStorage.save(topic4);
 
-        Hashtag hashtag1 = new Hashtag("#OOP");
-        hashtagStorage.save(hashtag1);
-        Hashtag hashtag2 = new Hashtag("#Java");
-        hashtagStorage.save(hashtag2);
+
 
     }
 
